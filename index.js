04c6901f8b01f3d3f6fc84c18d3d38585bcbcd90
BARIEEBA PRECIOUS DEEMENE <55707029-Prechluv@users.noreply.replit.com>
2026-04-11 14:27:39 +0000

    serve static files for docs.html

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+```js
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -35,6 +36,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
@@ -75,3 +77,4 @@ var PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', function() {
   console.log('Server running on port ' + PORT);
 });
+```
